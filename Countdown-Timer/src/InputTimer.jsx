0b8318089baa9f30aa1345712c2b4d4f8d257b22
SUMIT KUMAR <125842926+SumitKumar-2004@users.noreply.github.com>
2024-09-16 18:30:13 -0700

    Add files via upload

---
+++ Countdown-Timer/src/InputTimer.jsx
@@ -0,0 +1,26 @@
+const InputTimer = ({ handleInput, handleStart }) => {
+  return (
+    <div className="input-container">
+      <div className="input-box">
+        <input onChange={handleInput} type="text" id="hours" placeholder="HH" />
+        <input
+          onChange={handleInput}
+          type="text"
+          id="minutes"
+          placeholder="MM"
+        />
+        <input
+          onChange={handleInput}
+          type="text"
+          id="seconds"
+          placeholder="SS"
+        />
+      </div>
+      <button onClick={handleStart} className="timer-button">
+        Start
+      </button>
+    </div>
+  );
+};
+
+export default InputTimer;
