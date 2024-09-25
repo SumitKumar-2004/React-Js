d32f95a7cecfb0997e63998c380b6a166ebabf7d
SUMIT KUMAR <125842926+SumitKumar-2004@users.noreply.github.com>
2024-09-25 03:08:28 -0700

    Add files via upload

---
+++ Language-Translator/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Translate from "./components/Translate";
+function App() {
+  return (
+    <>
+      <h1 className="heading">Translator App</h1>
+      <Translate />
+    </>
+  );
+}
+
+export default App;
